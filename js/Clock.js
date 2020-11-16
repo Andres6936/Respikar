41e6e552625d3f09ad42afb6d3e92645c77a869d
Joan Andrés <andres6936@live.com>
2020-11-16 17:23:32 -0500

    Added const of proportion for hours, minutes and seconds

--- js/Clock.js
+++ js/Clock.js
@@ -1,6 +1,6 @@
 const inc = 1000;
 
-const WINDOW_WIDTH = window.screen.width;
+const WINDOW_WIDTH = document.documentElement.clientWidth;
 
 const wrapAnalogClock = document.querySelector('.wrap');
 const SIZE_WRAP_ELEMENT = WINDOW_WIDTH * 0.9;
@@ -9,17 +9,17 @@ wrapAnalogClock.style.width = String(SIZE_WRAP_ELEMENT) + 'px';
 wrapAnalogClock.style.height = String(SIZE_WRAP_ELEMENT) + 'px';
 
 const hourAnalogClock = document.querySelector('.hour');
-const SIZE_HOUR_ELEMENT = SIZE_WRAP_ELEMENT / 3.5;
+const SIZE_HOUR_ELEMENT = SIZE_WRAP_ELEMENT / 3.708154506;
 
 hourAnalogClock.style.height = String(SIZE_HOUR_ELEMENT) + 'px';
 
 const minuteAnalogClock = document.querySelector('.minute');
-const SIZE_MINUTE_ELEMENT = SIZE_WRAP_ELEMENT / 2.692307692;
+const SIZE_MINUTE_ELEMENT = SIZE_WRAP_ELEMENT / 2.632140137;
 
 minuteAnalogClock.style.height = String(SIZE_MINUTE_ELEMENT) + 'px';
 
 const secondAnalogClock = document.querySelector('.second');
-const SIZE_SECOND_ELEMENT = SIZE_WRAP_ELEMENT / 3.888888889;
+const SIZE_SECOND_ELEMENT = SIZE_WRAP_ELEMENT / 3.852842809;
 
 secondAnalogClock.style.height = String(SIZE_SECOND_ELEMENT) + 'px';
 
