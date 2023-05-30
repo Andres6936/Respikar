5a44efa1cc09eb44d8c55719d609362cb3c09b5c
Andres6936 <andres6936@gmail.com>
2023-05-30 14:03:32 -0500

    Prepare New Module

--- services/src/app.module.ts
+++ services/src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from "./entity/user.entity";
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: "sqlite",
+      database: 'respikar',
+      entities: [User],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ services/src/user/user.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from "@nestjs/common";
+
+@Injectable()
+export class UserService {
+    public save() {
+        
+    }
+    
+    public update() {
+        
+    }
+    
+    public delete() {
+        
+    }
+    
+    public getBy(){
+        
+    }
+    
+    public getAll() {
+        
+    }
+}
