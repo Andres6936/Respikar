799640aaab478fee79ec8ee98627f5e821c8d67d
Andres6936 <andres6936@gmail.com>
2023-06-03 12:17:28 -0500

    Added Service

--- services/src/user/user.service.ts
+++ services/src/user/user.service.ts
@@ -1,24 +1,28 @@
 import { Injectable } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Repository } from "typeorm";
+import { User } from "../entity/user.entity";
 
 @Injectable()
 export class UserService {
-    public save() {
-        
-    }
-    
-    public update() {
-        
-    }
-    
-    public delete() {
-        
-    }
-    
-    public getBy(){
-        
-    }
-    
-    public getAll() {
-        
-    }
+  constructor(
+    @InjectRepository(User)
+    private userRepository: Repository<User>
+  ) {}
+
+  public async save(user: User): Promise<User> {
+      return await this.userRepository.save(user);
+  }
+
+  public async  delete(id): Promise<void> {
+      await this.userRepository.delete(id);
+  }
+
+  public findOne(id: number): Promise<User | null> {
+    return this.userRepository.findOneBy({ id });
+  }
+
+  public getAll(): Promise<User[]> {
+    return this.userRepository.find();
+  }
 }
