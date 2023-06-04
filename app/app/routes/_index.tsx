da9d5e6d2f146f1053c4307964a7b676117bbacd
Andres6936 <andres6936@gmail.com>
2023-06-04 10:38:17 -0500

    Added Style

--- app/app/routes/_index.tsx
+++ app/app/routes/_index.tsx
@@ -10,8 +10,10 @@ export default function Index() {
   return (
     <div className="position:relative min-h:100vh bg:black bg:url('/img/background/BackgroundHome.jpg') background-size:cover background-repeat:no-repeat background-position:left|95%|bottom|45%">
       <div className="flex flex:row align-items:center position:absolute bottom:0 px:2em py:4em gap:1em">
-        <div className="flex flex:1 justify-content:center align-items:center w:5em h:5em r:50%">
-          <ChevronLeft size={36} />
+        <div className="flex flex:1">
+          <div className="flex justify-content:center align-items:center fg:white bd:blur(8px) bg:rgba(0,0,0,0.6) w:5em h:5em r:50%">
+            <ChevronLeft size={36} />
+          </div>
         </div>
 
         <div className="flex flex:3">
