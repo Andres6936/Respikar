d3e321507b29d689545efa560b0d9d9953a357f8
Andres6936 <andres6936@gmail.com>
2023-06-07 14:53:28 -0500

    Added Navigator

--- app/app/layout/BottomNavigator.tsx
+++ app/app/layout/BottomNavigator.tsx
@@ -1,4 +1,3 @@
-import {Link, useLocation} from "@remix-run/react";
 import SoundWave from "~/components/icons/Soundwave";
 import {useNavigate} from "react-router";
 import Bell from "~/components/icons/Bell";
@@ -7,20 +6,19 @@ import Gear from "~/components/icons/Gear";
 import Person from "~/components/icons/Person";
 
 export default function BottomNavigator() {
-    const location = useLocation();
     const navigator = useNavigate();
     
     return (
-        <footer className="position:absolute flex flex:3 flex:row aling-items:center bg:rgba(0,0,1,0.2) bd:blur(8px) r:43px p:0.2em bottom:1em left:1em right:1em">
+        <footer className="position:fixed flex flex:3 flex:row aling-items:center bg:rgba(0,0,1,0.2) bd:blur(8px) r:43px p:0.2em bottom:1em left:1em right:1em">
             <div className="flex justify-content:center align-items:center bg:white fg:black w:5em h:5em r:50%">
                 <SoundWave onClick={() => navigator("/signin")} size={36} />
             </div>
 
             <div className={"flex flex:row flex:1 justify-content:space-around"}>
-                <Bell size={24} className={"font:bold fg:white"}/>
-                <Calendar size={24} className={"font:bold fg:white"}/>
+                <Bell size={24} className={"font:bold fg:white"} onClick={() => navigator("/alarm")}/>
+                <Calendar size={24} className={"font:bold fg:white"} onClick={() => navigator("/howto")}/>
                 <Gear size={24} className={"font:bold fg:white"}/>
-                <Person size={24} className={"font:bold fg:white"}/>
+                <Person size={24} className={"font:bold fg:white"} onClick={() => navigator("/profile")}/>
             </div>
         </footer>
     )
